74643364156ae64f6b8e24220435ee9b9d73cc5a
bobsan42 <bobsan42s@gmail.com>
2023-04-02 23:50:24 +0300

    moreDOM

--- JS_front_end/04DOMandEvents/tasks_lab/09. Highlight-Active/app.js
+++ JS_front_end/04DOMandEvents/tasks_lab/09. Highlight-Active/app.js
@@ -1,3 +1,20 @@
 function focused() {
-    console.log('TODO:...');
+    // console.log('TODO:...');
+    const inputs = document.getElementsByTagName('input');
+    for (let i of inputs) {
+        i.addEventListener('focus', handler);
+        i.addEventListener('blur', handler);
+    }
+
+    function handler(e) {
+        // console.log(e.type);
+        const inputParent = e.target.parentElement;
+        // console.log(inputParent);
+        if (e.type === 'focus') {
+            inputParent.setAttribute('class','focused'); 
+        } else if (e.type === 'blur' ) {
+            inputParent.setAttribute('class',''); 
+        }
+    }
+
 }
